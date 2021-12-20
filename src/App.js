a6321dcae0d9418ce1c3f4e72f0f7c7b5736850d
tiarman <tiarmancr@gmail.com>
2021-12-20 15:09:27 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
-// import {
-// BrowserRouter,
-//   Route,
-//   Routes
-// } from "react-router-dom";
 import Home from './Components/Pages/Home';
 function App() {
   return (
-  //   <BrowserRouter>
-  //     <Routes >
-  //       <Route path="/user" element={<User />} />
-  //       <Route path="/users" element={<AllUserData />} />
-
-  //     </Routes>
-  // </BrowserRouter>
   <div>
     <Home/>
   </div>
